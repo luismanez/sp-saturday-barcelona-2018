29b408fd5e7c8b550be4e8f262c5b8327fa3e78b
luismanez <luis.manez@outlook.com>
2018-10-26 10:29:58 +0200

    latest changes before session

---
+++ demos/live/SPSBcn.GraphDemo/GraphGetUsersV2Endpoint/AzureActiveDirectorySettings.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace GraphGetUsersV2Endpoint
+{
+    public class AzureActiveDirectorySettings
+    {
+        public string Tenant { get; set; }
+        public string ClientId { get; set; }
+        public string Secret { get; set; }
+        public string Authority { get; set; }
+        public string ReturnUri { get; set; }
+
+        public static AzureActiveDirectorySettings Initialize()
+        {
+            return JsonConvert.DeserializeObject<AzureActiveDirectorySettings>(
+                File.ReadAllText(@"C:\Github\sp-saturday-barcelona-2018\demos\done\SPSBcn.GraphDemo\secrets.app3.json"));
+        }
+    }
+}
